5ea04224918c6ff522abf0a0147b62ceb9743b6c
rukubaruku <vogliarmant@gmail.com>
2025-08-01 10:18:09 +0200

    cors fix

--- app.js
+++ app.js
@@ -25,14 +25,6 @@ const corsOptions = {
 app.use(cors(corsOptions));
 app.options("*", cors(corsOptions));
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://teater.arkiva.gov.al");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  res.header("Access-Control-Allow-Credentials", "true");
-  next();
-});
-
 app.use(express.json());
 
 app.get("/", (req, res) => {
